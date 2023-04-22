d1f8acc83343380d15fd7016517d6a60f5275827
tomshmueli <tomshmueli@mail.tau.ac.il>
2023-04-22 12:56:22 +0300

    code_wrriten

--- hw0_tom_shahar.c
+++ hw0_tom_shahar.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 
+int i = 0;
+int sum = 0;
 
 int inputCheck(char t)
 {
     int x = scanf("%d");
-    if (x == 1) {
+    if (x == 1) {   // recived integer
         if (1<x<17)
         {
         return x;
@@ -18,35 +20,53 @@ int inputCheck(char t)
     else{
         printf("Invalid target base!\n");
     }
-    return NULL
+    return NULL;
 }
-    
-    
-    
 
-int i = 0;
-int sum = 0;
-void reverse()
+void toTarget(int target)
+{
+    int divide = sum%target;
+    if (divide<10)
+    {
+        printf("%d", divide);
+    }
+    else
+    {
+        char c = 97 + divide - 10;
+        printf("%c", c);
+    }
+    sum = floor((float)sum/target);
+}
+
+void toDecimal(int source)
 {
     char c;
     if ((c = getchar()) != '\n')
     {
-        reverse();
-        sum += (c - '0') * pow(2, i);
+        toDecimal(source);
+        if ('a'<=c<='f')
+        {
+            sum += (c - 'a' + 10) * pow(source, i);
+        }
+        else{
+            sum += (c - '0') * pow(source, i);
+        }   
         i++;
     }
-    return;
 }
-int main()
+
+void main()
 {
     int source;
     int target;
+    int decimal;
+    int ret;
     printf("Enter the source base:\n");
     source = inputCheck('s');
     printf("Enter the target base:\n");
     target = inputCheck('t');
-    printf("Enter a Binary number:\n");
-    reverse();
-    printf("The Decimal is: %d\n", sum);
-    return 0;
+    printf("Enter a number in base %d:\n", source);
+    toDecimal(source);
+    printf("The number in base %d is:", target);
+    toTarget(target);
 }
